807a35c8abfc6ba83b9c0b2b132c59e17751b59e
Miguel Urena <urena_miguel82@outlook.com>
2024-05-30 10:49:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -49,10 +49,11 @@ app.use(passport.session());
 // Connect flash middleware
 app.use(flash());
 
-// Middleware to set user for views
+// Global variables for flash messages
 app.use((req, res, next) => {
-  res.locals.messages = req.flash();
-  res.locals.user = req.user || null;
+  res.locals.success_msg = req.flash('success');
+  res.locals.error_msg = req.flash('error');
+  res.locals.error = req.flash('error');
   next();
 });
 
