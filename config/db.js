ba8138b53cd87a6042df7893a8d2126c52209d13
Miguel Urena <urena_miguel82@outlook.com>
2024-05-30 13:01:51 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      userNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
