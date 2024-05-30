47fe1283ef50857fc5da9c26e54856f95e41c337
Miguel Urena <urena_miguel82@outlook.com>
2024-05-30 13:05:30 -0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -40,6 +40,11 @@ module.exports = function (passport) {
   });
 
   passport.deserializeUser(async (id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
   });
 };
