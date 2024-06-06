91837a356639a1d2e0b92a6cc0f9a931c366be6c
Miguel Urena <urena_miguel82@outlook.com>
2024-06-06 13:17:17 -0700

    Bug fix

--- app.js
+++ app.js
@@ -64,12 +64,12 @@ app.use('/blogs', require('./routes/blogs'));
 
 // Error handling middleware
 app.use((req, res, next) => {
-  res.status(404).render('error/404');
+  res.status(404).render('error/404', { user: req.user });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).render('error/500');
+  res.status(500).render('error/500', { user: req.user });
 });
 
 const PORT = process.env.PORT || 3000;
